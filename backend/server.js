78d4aa9c69a39a9dc6e4ae8a18ba60147ac61f11
Jose Alvarez <rosney.sotor@gmail.com>
2024-05-07 01:18:21 -0300

    Se revierten cambios

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const PORT = 8080;
 const bodyParser = require("body-parser");
-const db = require('../db');
-const routes = require('../routes/routes');
+const db = require('./db');
+const routes = require('./routes/routes');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const { engine } = require('express-handlebars');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const serverless = require('serverless-http');
 
 db(process.env.MONGODB_DATABASE);
 
@@ -44,8 +43,7 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.use('/.netlify/functions/server', routes);
-module.exports.handler = serverless(app);
+app.use('/', routes);
 
 app.get('/', (req, res) => {
     res.send({
